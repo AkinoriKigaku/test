1f362360f17d030d9794021f8731d0323b5dd30c
AkinoriKigaku <vida_plena_de_alegriayfelicidad@yahoo.co.jp>
2020-11-01 15:35:45 +0900

    モジュール DrawingSearch の初期チェックイン

---
+++ DrawingSearch/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DrawingSearch.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        //public void OnGet()
+        //{
+
+        //}
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+            return Redirect("/ButtonComplete");
+        }
+    }
+}
